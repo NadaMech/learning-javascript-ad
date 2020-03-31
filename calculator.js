46bdaa6a2f6d791c2c7b1d25082645e9b2058b99
Adan Fragoso <adan.carrera.fragoso@outlook.com>
2020-03-31 14:34:03 -0500

    First installation

---
+++ calculator.js
@@ -0,0 +1,13 @@
+"use strict"; 
+//functions will be read below here
+
+function adder(number1,number2)
+{
+    let add1 = prompt("You've called the adder function- insert first number here", ); //the numbers will stand for the first, second, third, etc number you punch in
+
+    let add2 = prompt("Instert the next number here", );
+    
+    return alert(parseint(add1) + parseint(add2));
+};
+
+adder();
