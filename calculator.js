c816d9549562ca66de20b8a753f331ee054b8068
AFC <53988553+NadaMech@users.noreply.github.com>
2020-03-31 18:02:54 -0500

    Updated calculator

--- calculator.js
+++ calculator.js
@@ -3,11 +3,23 @@
 
 function adder(number1,number2)
 {
-    let add1 = prompt("You've called the adder function- insert first number here", ); //the numbers will stand for the first, second, third, etc number you punch in
-
-    let add2 = prompt("Instert the next number here", );
+    let number1 = prompt("You've selected the add property- please insert first number",);
     
-    return alert(parseint(add1) + parseint(add2));
+    if(number1 = NaN)
+    {
+        alert("Enter number please");
+        adder();
+        return 0;
+    }
+
+    let number2 = prompt("Insert the next number to add",);
+
+    if(number2 = NaN)
+    {
+        alert("Enter number please");
+        adder();
+        return 0;
+    }
 };
 
 adder();
